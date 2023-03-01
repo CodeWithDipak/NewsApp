550b7ba13359fb8859d8a99cef541c7986342f44
Dipak_kasar <kasardipak333@gmail.com>
2023-03-01 13:36:10 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Link} from "react-router-dom"
 
-export default class NavBar extends Component {
-    render() {
+export default function NavBar() {
+    
         return (
             <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
                 <div className="container-fluid">
@@ -25,5 +25,5 @@ export default class NavBar extends Component {
                 </div>
             </nav>
         )
-    }
+ 
 }

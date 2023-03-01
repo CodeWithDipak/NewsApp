99ef4a85c61a710ccb06778bbae322c368a1d563
Dipak_kasar <kasardipak333@gmail.com>
2023-03-01 13:36:01 +0530

    Update NewsItems.js

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-
-export default class NewsItems extends Component {
-    render() {
+ const NewsItems = (props)=> {
+   
         return (
             <div className='my-2'>
                 <div className="card" >
-                    <img src={!this.props.imgUrl ? "https://cdn4.iconfinder.com/data/icons/ui-beast-4/32/Ui-12-512.png" : this.props.imgUrl} className="card-img-top" alt="..." />
+                    <img src={!props.imgUrl ? "https://cdn4.iconfinder.com/data/icons/ui-beast-4/32/Ui-12-512.png" : props.imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{this.props.title}</h5>
-                        <p className="card-text">{this.props.description}</p>
-                        <p className="card-text"><small className="text-muted">Released on {new Date(this.props.date).toGMTString()}</small></p>
-                        <a href={this.props.newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.description}</p>
+                        <p className="card-text"><small className="text-muted">Released on {new Date(props.date).toGMTString()}</small></p>
+                        <a href={props.newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
         )
-    }
 }
+
+export default NewsItems
